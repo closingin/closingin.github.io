8ad3e9273f5be1517030ac3247d0ac7cb3575080
closingin <2735603+closingin@users.noreply.github.com>
2017-07-15 15:45:03 +0200

    Changed the structure and the colors, added animations

--- public/app.js
+++ public/app.js
@@ -3,15 +3,23 @@
 */
 
 (function() {
+    var description = document.querySelector('#description');
+    description.style.opacity = 1;
+    description.style.transform = 'translateY(0)';
+
+    var links = document.querySelector('#links');
+    links.style.opacity = 1;
+    links.style.transform = 'translateY(0)';
+
     var projects = document.querySelector('#projects > div');
     projects.style.opacity   = 1;
-    projects.style.transform = 'translateX(0)';
+    projects.style.transform = 'translateY(-50%)';
 
     var useful = document.querySelector('#useful > div');
     useful.style.opacity   = 1;
-    useful.style.transform = 'translateY(0)';
+    useful.style.transform = 'translateY(-50%)';
 
     var useful = document.querySelector('#skills > div');
     useful.style.opacity   = 1;
-    useful.style.transform = 'translateY(0)';
+    useful.style.transform = 'translateY(-50%)';
 })();
