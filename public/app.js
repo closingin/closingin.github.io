87f392787977f318975537681f9d87d714999e6a
closingin <2735603+closingin@users.noreply.github.com>
2017-07-15 14:47:07 +0200

    First commit

---
+++ public/app.js
@@ -0,0 +1,17 @@
+/*
+** Written by closingin.
+*/
+
+(function() {
+    var projects = document.querySelector('#projects > div');
+    projects.style.opacity   = 1;
+    projects.style.transform = 'translateX(0)';
+
+    var useful = document.querySelector('#useful > div');
+    useful.style.opacity   = 1;
+    useful.style.transform = 'translateY(0)';
+
+    var useful = document.querySelector('#skills > div');
+    useful.style.opacity   = 1;
+    useful.style.transform = 'translateY(0)';
+})();
