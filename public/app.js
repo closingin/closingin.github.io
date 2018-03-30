28e18fd0097141dacaf1965722c0dee7e2d00fc9
closingin <2735603+closingin@users.noreply.github.com>
2018-03-30 18:18:17 -0400

    From the dark to the bright side. Added contact section.

--- public/app.js
+++ public/app.js
@@ -2,6 +2,18 @@
 ** Written by closingin.
 */
 
+function blur(elements) {
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].classList.add('faded');
+    }
+}
+
+function unblur(elements) {
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].classList.remove('faded');
+    }
+}
+
 (function() {
     var description = document.querySelector('#description');
     description.style.opacity = 1;
@@ -14,25 +26,45 @@
     // var projects = document.querySelector('#projects > div');
     // projects.style.opacity   = 1;
     // projects.style.transform = 'translateY(-50%)';
-    //
-    // var useful = document.querySelector('#useful > div');
-    // useful.style.opacity   = 1;
-    // useful.style.transform = 'translateY(-50%)';
-    //
-    // var useful = document.querySelector('#skills > div');
-    // useful.style.opacity   = 1;
-    // useful.style.transform = 'translateY(-50%)';
 
+    var education = document.querySelector('#education > div');
+    education.style.opacity   = 1;
+    education.style.transform = 'translateY(0)';
+
+    var contact = document.querySelector('#contact > div');
+    contact.style.opacity   = 1;
+    contact.style.transform = 'translateY(0)';
+
+    var menu        = document.querySelector('header nav');
+    var menuItems   = document.querySelectorAll('header nav a');
     var menuToggler = document.querySelector('header .menu-toggle');
-    var menu = document.querySelector('header nav');
+
+    var sections = document.querySelectorAll('section');
+
+    for (var i = 0; i < menuItems.length; i++) {
+        menuItems[i].addEventListener('click', function(e) {
+            e.preventDefault();
+
+            menu.classList.remove('active');
+            menuToggler.classList.remove('active');
+
+            unblur(sections);
+
+            document.querySelector(e.target.hash).scrollIntoView({
+                behavior: 'smooth'
+            });
+        })
+    }
 
     menuToggler.addEventListener('click', function(e) {
         if (menu.classList.contains('active')) {
             menu.classList.remove('active');
             menuToggler.classList.remove('active');
+            unblur(sections);
         } else {
             menu.classList.add('active');
             menuToggler.classList.add('active');
+            blur(sections);
         }
     });
 })();
